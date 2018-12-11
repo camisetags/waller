de1d70440715048d1894ae1ea1390fe86fa980eb
gabrielSeixas <salesseixasgabriel@gmail.com>
2018-12-11 18:31:16 -0200

    Fixed Walls form wall repo

--- lib/waller/walls/walls.ex
+++ lib/waller/walls/walls.ex
@@ -10,16 +10,17 @@ defmodule Waller.Walls do
   alias Waller.Participants.User
 
   def form_wall(wall_params) do
-    # wall = Wall.changeset(%Wall{}, wall_params)
     # users = User.changeset(%User{}, users_params)
     # Ecto.Changeset.put_assoc(wall, :users, users)
     # |> Repo.insert
-    wall = Repo.preload(wall_params, :users)
+    
+    wall = Wall.changeset(%Wall{}, wall_params)
+    wall = Repo.preload(wall, :users)
     users = wall.users |> Enum.map(&Ecto.Changeset.change/1)
 
     wall
     |> Ecto.Changeset.change
     |> Ecto.Changeset.put_assoc(:users, users)
-    |> Repo.insert!
+    |> Repo.insert()
   end
 end
